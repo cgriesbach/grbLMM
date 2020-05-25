1361c044a7ffdaa1aba83c54d451fce56bbc68f7
cgriesbach <colin.griesbach@fau.de>
2020-05-25 09:10:53 +0200

    AIC added.

--- R/grbLMM.R
+++ R/grbLMM.R
@@ -1,12 +1,13 @@
-library(nlme)
+library(lme4)
 library(parallel)
 library(Matrix)
 
-grbLMM = function(y, X, Z, id, m.stop = 500, ny = .1, cv.dat = NULL){
+grbLMM = function(y, X, Z, id, m.stop = 500, ny = .1, cv.dat = NULL, aic = FALSE){
   getV = function(x){solve(x/sigma2 + Qi)}
   getQ = function(x, y){x + tcrossprod(as.numeric(y))}
   cfc = function(x)(length(unique(x)))
-
+  getZ2D = function(x){chol2inv(chol(x + D))}
+  
   ### basic definitions
   id = as.numeric(factor(id, levels = unique(id)))
   N = length(id)
@@ -18,55 +19,69 @@ grbLMM = function(y, X, Z, id, m.stop = 500, ny = .1, cv.dat = NULL){
   }
   p = ncol(X)
   q = ncol(Z)
-
+  
   ### extract cluster-constant covariates
   ccc = rep(FALSE, p)
   for(r in 1:p){
     if(Reduce('+', lapply(split(X[,r], id), cfc)) == n){ccc[r] = TRUE}
   }
   Xcc = cbind(1, X[first, ccc])
-  Xsw = cbind(X[, ccc], Z[,-1])
+  Xsw = X[,ccc]
   Xcor = list()
   Xcor[[1]] = Xcc%*%solve(crossprod(Xcc))%*%t(Xcc)
-
+  
   if(q>1){
     for(s in 2:q){
       x = matrix(rep(1, n), n, 1)
       Xcor[[s]] = x%*%solve(crossprod(x))%*%t(x)
     }
   }
-
+  
   p1 = rep(seq(1, q*n, q), q) + rep(0:(q-1), each = n)
   p2 = rep(seq(1, q*n, n), n) + rep(0:(n-1), each = q)
   P1 = sparseMatrix(seq_along(p1), p1)
   P2 = sparseMatrix(seq_along(p2), p2)
-
+  
   Xcor = bdiag(Xcor)
   Xcor = P2%*%(diag(n*q) - Xcor)%*%P1
-
+  
   ### construct random effects design matrix
   Z0 = Z
   Z = Z2 = QQ = list()
   for(i in 1:n){
-    Z[[i]] = Z0[id==i,]
+    if(id.t[i] == 1){
+      Z[[i]] = matrix(Z0[id==i,], 1, q)
+    }else{
+      Z[[i]] = Z0[id==i,]
+    }
     Z2[[i]] = crossprod(Z[[i]])
   }
   bZ = Z
   Z = bdiag(Z)
-
-  ### set starting values
+  Z22 = crossprod(Z)
+  
+  ##### set starting values
   beta = rep(0, p)
-
+  
+  ### offset model for intercept and random structure
   if(q==1){
-    offset = lme(y ~ Xsw, random = ~ 1 | id, control = lmeControl(opt = "optim", singular.ok = TRUE, returnObject = TRUE))
+    offset = lme(y ~ 1, random = ~ 1 | id, control = lmeControl(opt = "optim", singular.ok = TRUE, returnObject = TRUE))
   }else{
-    offset = lme(y ~ Xsw, random = ~ Z0[,-1] | id, control = lmeControl(opt = "optim", singular.ok = TRUE, returnObject = TRUE))
+    offset = lme(y ~ 1, random = ~ Z0[,-1] | id, control = lmeControl(opt = "optim", singular.ok = TRUE, returnObject = TRUE))
   }
+  
+  ### extract starting values
   int = offset$coefficients$fixed[1]
-  gamma = as.numeric(offset$coefficients$random$id, byrow = TRUE)
+  gamma = Xcor%*%as.numeric(t(offset$coefficients$random$id))
   sigma2 = offset$sigma^2
   Q = getVarCov(offset)
-
+  
+  ### construct initial hat matrix
+  C = cbind(1, Z)
+  G = kronecker(diag(n), Q)
+  B = as.matrix(bdiag(diag(1), solve(G)))
+  S = list(C%*%bdiag(1,Xcor)%*%solve(crossprod(C) + sigma2*B)%*%t(C))
+  
   ### in case of cv
   clcv = NA
   if(!is.null(cv.dat)){
@@ -74,7 +89,7 @@ grbLMM = function(y, X, Z, id, m.stop = 500, ny = .1, cv.dat = NULL){
     Ncv = length(idcv)
     ncv = length(unique(idcv))
     idcv.t = as.numeric(table(idcv))
-
+    
     Zcv = list()
     for(i in 1:ncv){
       if(idcv.t[i] == 1){
@@ -84,22 +99,25 @@ grbLMM = function(y, X, Z, id, m.stop = 500, ny = .1, cv.dat = NULL){
         Zcv[[i]] = cv.dat$Zcv[idcv==i,]
       }
     }
-
+    
     Zcv = as.matrix(bdiag(Zcv))
   }
-
+  
   ### prepare baselearners
-  BL = list()
+  BL = HM = list()
   for(r in 1:p){
     x = cbind(1, X[,r])
     BL[[r]] = solve(crossprod(x))%*%t(x)
+    HM[[r]] = ny*x%*%BL[[r]]
   }
-
-  Z22 = crossprod(Z)
-
+  
   ### define storing matrices/vectors
-  INT = BETA = GAMMA = SIGMA2 = CLCV = c()
-
+  INT = int
+  BETA = beta
+  GAMMA = gamma
+  SIGMA2 = sigma2
+  CLCV = l = AIC = AICc = c()
+  
   for(m in 1:m.stop){
     ###############################################################
     #### S1 #######################################################
@@ -112,7 +130,7 @@ grbLMM = function(y, X, Z, id, m.stop = 500, ny = .1, cv.dat = NULL){
       fit = BL[[r]]%*%u
       fits[1,r] = fit[1]
       fits[2,r] = fit[2]
-      fits[3,r] = sum((y-cbind(1, X[,r])%*%fit)^2)
+      fits[3,r] = sum((u - cbind(1, X[,r])%*%fit)^2)
     }
 
     best = which.min(fits[3,])
@@ -126,91 +144,100 @@ grbLMM = function(y, X, Z, id, m.stop = 500, ny = .1, cv.dat = NULL){
     eta = as.vector(int + X%*%beta + Z%*%gamma)
     u = y - eta
 
-    Qi = solve(Q)
-    D = kronecker(diag(n), Qi)*sigma2
-    rfit = chol2inv(chol(Z22 + D))%*%t(Z)%*%u
-    #rfit = chol2inv(chol(Z22 + D))%*%t(Z)%*%u
-    gamma = Xcor%*%(gamma + ny*rfit)
+    D = solve(Q/sigma2)
+    Z2D = lapply(Z2, getZ2D)
+    rfit = ny*Xcor%*%bdiag(Z2D)%*%t(Z)
+    gamma = gamma + rfit%*%u
 
     ###############################################################
     #### S3 #######################################################
     ###############################################################
     eta = as.vector(int + X%*%beta + Z%*%gamma)
-
+    
+    Qi = solve(Q)
     sigma2 = var(y - eta)
 
     V = lapply(Z2, getV)
     V = mapply(getQ, V, split(gamma, rep(1:n, each = q)), SIMPLIFY = FALSE)
     Q = Reduce("+", V)/n
 
-    ### predicitve risk computation
+    ### predictive risk computation
     if(!is.null(cv.dat)){
       Qcv = diag(Ncv) + Zcv%*%kronecker(diag(ncv), Q/sigma2)%*%t(Zcv)
-      clcv = t(cv.dat$ycv - cv.dat$Xcv%*%beta)%*%chol2inv(chol(Qcv))%*%(cv.dat$ycv - cv.dat$Xcv%*%beta)/ncv
+      clcv = mean((cv.dat$ycv - int - cv.dat$Xcv%*%beta)^2)
     }
 
+    if(aic){
+      Sma = HM[[best]]
+      Smb = Z%*%rfit
+      S[[m+1]] = diag(N) - (diag(N) - Smb)%*%(diag(N) - Sma)%*%(diag(N) - S[[m]])
+      df = sum(diag(S[[m+1]]))
+      s2 = mean((y - eta)^2)
+      AICc[m] = log(s2) + (1 + df/N) / (1 - (df + 2)/N)
+    }
 
     INT = c(INT, int)
     BETA = rbind(BETA, beta)
-    GAMMA = rbind(GAMMA, gamma)
+    GAMMA = cbind(GAMMA, gamma)
     SIGMA2 = c(SIGMA2, sigma2)
     QQ[[m]] = Q
     CLCV = c(CLCV, clcv)
 
     if(m%%10 == 0){print(m)}
   }
-
-  structure(list(int = int, beta = beta, sigma2 = sigma2, gamma = gamma, Q = Q,
-                 INT = INT, BETA = BETA, SIGMA2 = SIGMA2, GAMMA = GAMMA, QQ = QQ, CLCV = CLCV))
+  
+  structure(list(int = int, beta = beta, sigma2 = sigma2, gamma = gamma, Q = Q, AICc = AICc,
+                 INT = INT, BETA = BETA, SIGMA2 = SIGMA2, GAMMA = t(GAMMA), QQ = QQ, CLCV = CLCV,
+                 S = S, eta = eta))
 }
 
 cv.grbLMM = function(k, y, X, Z, id, m.stop = 500, ny = .1, cores = 1){
   id = as.numeric(factor(id, levels = unique(id)))
   id.t = as.numeric(table(id))
   n = length(id.t)
-
+  
   ### create an equally sized partition
   sets = sample(cut(1:n, k, labels = F))
   sets = rep(sets, id.t)
-
+  
   ### function for each fold
   k.fold = function(k){
     y_train = y[sets != k]
     X_train = X[sets != k,]
     Z_train = as.matrix(Z[sets != k,])
     id_train = id[sets != k]
-
+    
     cv.dat = list('ycv' = y[sets == k],
                   'Xcv' = X[sets == k,],
                   'Zcv' = as.matrix(Z[sets == k,]),
                   'idcv' = id[sets == k])
-
+    
     model = grbLMM(y_train, X_train, Z_train, id_train, m.stop = m.stop, ny = .1, cv.dat = cv.dat)
-
+    
     return(model$CLCV)
   }
-
+  
   ### execute model on all folds
   if(cores==1){
     cv.ls = lapply(1:k, k.fold)
   }else{
     cv.ls = mclapply(1:k, k.fold, mc.cores = cores)
   }
-
+  
   ### find best performing m.stop averaged over all folds
   cv.MAT = c()
   for(i in 1:k){
     cv.MAT = rbind(cv.MAT, cv.ls[[i]])
   }
-
+  
   pred.risk = colMeans(cv.MAT)
   m.opt = which.min(pred.risk)
-
+  
   model = grbLMM(y, X, Z, id, m.stop = m.opt)
   model$m.opt = m.opt
   model$pred = pred.risk
   model$coef = c(model$int, model$beta)
   model$folds = cv.MAT
-
+  
   return(model)
 }
